7ba5919981046d2282e84b7b142a2ab2ae28ef4f
MadobyPy <Madoby.AI@gmail.com>
2024-06-02 01:31:10 +0100

    Update index.js - 5
    -Delays: (Randomized)
    between 80secs and 120secs: (Pauses during the execution)
    ~10sec (between the sessions)
    
    -UserAgent

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
 		"Content-Type": "application/json",
 		Authorization: `Bearer ${process.env.DUOLINGO_JWT}`,
 		"user-agent":
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.17 Safari/537.36",
 	};
 
 	const { sub } = JSON.parse(
